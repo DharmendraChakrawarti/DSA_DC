d7b3a8126cadfcbbca5a961ec75c7defb4eb474a
Dharmendra Chakrawarti <dharmendrachakrawarti9657@gmail.com>
2022-03-26 16:10:42 +0530

    fyg

---
+++ Recursion/R_5_SubSequence.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void printF(vector<int> &vec,int arr[],int n,int index){
+    if(index==n){
+        for(auto i:vec){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        return;}
+        //take or pick the element 
+        vec.push_back(arr[index]);
+        printF(vec,arr,n,index+1);
+        vec.pop_back();
+
+        //to do not take element or remove
+        printF(vec,arr,n,index+1); 
+}
+int main(){
+    int arr[]={2,4,5};
+    vector<int> vec;
+    printF(vec,arr,3,0);
+    return 0;
+}
