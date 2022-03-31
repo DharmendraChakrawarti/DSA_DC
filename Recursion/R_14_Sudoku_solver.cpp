d3fbede5bc340f8498e38174f45395806794b1e4
Dharmendra Chakrawarti <dharmendrachakrawarti9657@gmail.com>
2022-03-31 23:38:47 +0530

    dc

---
+++ Recursion/R_14_Sudoku_solver.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool issaf(int row, int col, vector<vector<char>> &ans, char c)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (ans[i][col] == c)
+            return false;
+        if (ans[row][i] == c)
+            return false;
+        if (ans[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+            return false;
+    }
+    return true;
+}
+bool solve(vector<vector<char>>& ans)
+{
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[0].size(); j++)
+        {
+            if (ans[i][j] == '.')
+            {
+                for (char c = '1'; c <= '9'; c++)
+                {
+                    if (issaf(i, j, ans, c))
+                    {
+                        ans[i][j] = c;
+                        if (solve(ans))
+                        {
+                            return true;
+                        }
+                        else
+                            ans[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void sudoku(vector<vector<char>> &ans)
+{
+    solve(ans);
+}
+int main()
+{
+    // https://leetcode.com/problems/sudoku-solver/
+    vector<vector<char>> ans(9, vector<char>(9,'.'));
+    sudoku(ans);
+    for (auto i : ans)
+    {
+        for (auto j : i)
+        {
+            cout << j<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
